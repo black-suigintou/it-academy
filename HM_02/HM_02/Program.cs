21fae93ed31ad2454ce839e340fd7409b2ba64c2
Zinina Darya <black_suigintou@yahoo.com>
2019-07-06 17:20:38 +0300

    06/07/19

---
+++ HM_02/HM_02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 43.6F; // явное преобразование типов
+            char num2;
+            num2 = (char)num1;
+
+            int num3 = 29; // явное преобразование типов
+            short num4;
+            num4 = (short)num3;
+
+            double num5 = 204.4689; // явное преобразование типов
+            long num6;
+            num6 = (long)num5;
+
+            byte num7 = 76; // неявное преобразование типов
+            int num8 = num7;
+
+            float num9 = 21.45F; //  неявное преобразование типов
+            double num10 = num9;
+
+            uint num11 = num2;  //  неявное преобразование типов
+
+            double num12 = 2.46; //boxing
+            object x = num12;
+
+            double y = (double)x; //unboxing
+
+
+        }
+        
+        
+    }
+}
