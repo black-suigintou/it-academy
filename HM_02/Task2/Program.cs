a883da364677ea3490208ef5d561e59e6d660c0a
Zinina Darya <black_suigintou@yahoo.com>
2019-07-08 00:12:37 +0300

    Done 50/50

---
+++ HM_02/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class SystemType 
+    {
+        static void Main()
+        {
+            string a = "Hello, world!";
+            Console.WriteLine(a.GetType());
+
+            System.Byte b = 234;
+            Console.WriteLine(b.GetType());
+
+            Console.ReadLine();
+        }
+    }
+}
