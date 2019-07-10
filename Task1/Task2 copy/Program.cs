c6777ec0da9457ce45567b50d8635470b76454ee
Zinina Darya <black_suigintou@yahoo.com>
2019-07-10 23:19:17 +0300

    Working variants are Task1 and Task2 copy

---
+++ Task1/Task2 copy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_copy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Для передвижения по полю используйте клавиши W,A,S и D\nW-движение вверх, S-вниз, A-влево, D-вправо.\nДля выхода нажмите E.");
+            for (int i = 0; ;)
+            {
+                i++;
+                string letter = Console.ReadLine();
+                foreach (char way in letter)
+                {
+                    if (way == 'W' || way == 'w')
+                    {
+                        Console.WriteLine("Вверх");
+                    }
+                    else if (way == 'S' || way == 's')
+                    {
+                        Console.WriteLine("Вниз");
+                    }
+                    else if (way == 'A' || way == 'a')
+                    {
+                        Console.WriteLine("Влево");
+                    }
+                    else if (way == 'D' || way == 'd')
+                    {
+                        Console.WriteLine("Вправо");
+                    }
+                    else if (way == 'E' || way == 'e')
+                    {
+                        Console.WriteLine("Вы завершили");
+                        break;
+                    }
+
+
+                }
+
+               
+            }
+
+                             
+        }
+          
+
+    }
+}
---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Для передвижения по полю используйте клавиши W,A,S и D\nW-движение вверх, S-вниз, A-влево, D-вправо.\nДля выхода нажмите E.");
+
+            for (int i = 0; ;)
+            {
+                i++;
+                char letter;
+                letter = Convert.ToChar(Console.ReadLine()); //на этом месте при повторном вводе у меня выдает ошибку, как ее исправить так и не нашла
+                int num;
+                num = (int)letter;
+
+                if (num == 87 | num == 119)
+                {
+                    Console.WriteLine("Вверх");
+                }
+                else if (num == 83 | num == 115)
+                {
+                    Console.WriteLine("Вниз");
+                }
+                else if (num == 65 | num == 97)
+                {
+                    Console.WriteLine("Влево");
+                }
+                else if (num == 68 | num == 100)
+                {
+                    Console.WriteLine("Вправо");
+                }
+                else if (num == 69 | num == 101)
+                {
+                    break;                    
+                }
+
+                Console.Read();
+
+
+
+                               
+
+
+            }
+            
+            
+
+            
+            
+            
+        }
+    }
+}
